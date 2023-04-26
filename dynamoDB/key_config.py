137cce865e5ee915adee16d8a0a1ddf60f638b06
Rahul Singh nagesh <98824774+Hexton09@users.noreply.github.com>
2023-04-27 00:59:03 +0530

    Update key_config.py

--- dynamoDB/key_config.py
+++ dynamoDB/key_config.py
@@ -3,16 +3,9 @@
 Created on Thu Jul  9 19:56:55 2020
 
 @author: hp
-"""
 
-
-<<<<<<< HEAD
 ACCESS_KEY_ID='AKIARTCIRVA3LYI7GIP5'
 ACCESS_SECRET_KEY='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'
-=======
-ACCESS_KEY_ID=''
-ACCESS_SECRET_KEY=''
->>>>>>> ba552bc418bcca72e651bdc3d2b8547f39ab2cfc
 AWS_DEFAULT_REGION='ap-south-1'
 
 
