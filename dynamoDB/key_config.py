fd1710d27f6149379dfe281ed3e04d6f7f3574ae
Rahul Singh nagesh <98824774+Hexton09@users.noreply.github.com>
2023-04-27 01:03:09 +0530

    Update key_config.py

--- dynamoDB/key_config.py
+++ dynamoDB/key_config.py
@@ -1,15 +1,5 @@
-# -*- coding: utf-8 -*-
-Created on Thu Jul  9 19:56:55 2020
-
-@author: hp
 
 ACCESS_KEY_ID='AKIARTCIRVA3LYI7GIP5'
 ACCESS_SECRET_KEY='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'
 AWS_DEFAULT_REGION='ap-south-1'
 
-
-
-
-
-
-#BUCKET_NAME='unigrade-test-bucket-test'
